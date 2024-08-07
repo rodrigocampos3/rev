331cb5cc25f3dd8ac8a88d4590c2058e25a08226
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-08-07 13:36:55 -0300

    ajustes

--- revitaliza/src/app/page.tsx
+++ revitaliza/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         </div>
         <div className="w-full flex flex-col gap-2 md:gap-5 md:p-10 order-2 md:order-1">
           <h1 className="text-3xl md:text-5xl font-bold text-center text-[#EB591E] md:text-left">Explore Oportunidades e Impacto Social em Nossa Comunidade</h1>
-          <p className="text-center md:text-left px-2">aisfhaof soaifhaoisfa fiahsfioaf aoisfoaifoai iefowf aoishfoaifs asoifaosfabfoiasfuo asfgoauf asufgaofbgao faogsfuoab augfaof ausgfaobsfugoa fsagusofasf guoasf</p>
+          <p className="text-center md:text-left px-2 text-black">aisfhaof soaifhaoisfa fiahsfioaf aoisfoaifoai iefowf aoishfoaifs asoifaosfabfoiasfuo asfgoauf asufgaofbgao faogsfuoab augfaof ausgfaobsfugoa fsagusofasf guoasf</p>
           <div className="w-full py-4 md:py-10 px-5">
             <button className="bg-[#EB591E] text-xl text-white rounded-md w-full md:w-1/3 p-2">Saiba mais</button>
           </div>
@@ -35,7 +35,7 @@ export default function Home() {
             <span className="text-[#0247AB]">Constru</span>
             <span className="text-[#EB591E]">Favela</span>
           </h1>
-          <p>A ConstruFavela 2024 é um evento dedicado à construção civil nas comunidades, que ocorrerá nos dias 24 e 25 de outubro no Pavilhão Social do G10 Favelas, em Paraisópolis, São Paulo. O evento oferece uma oportunidade única para se conectar com um público de alto potencial econômico, com um mercado estimado em 167 bilhões de reais. Além de fomentar o crescimento profissional através de um curso gratuito de Técnicas Básicas da Construção Civil, a ConstruFavela promove networking com especialistas e expositores renomados.
+          <p className="text-black">A ConstruFavela 2024 é um evento dedicado à construção civil nas comunidades, que ocorrerá nos dias 24 e 25 de outubro no Pavilhão Social do G10 Favelas, em Paraisópolis, São Paulo. O evento oferece uma oportunidade única para se conectar com um público de alto potencial econômico, com um mercado estimado em 167 bilhões de reais. Além de fomentar o crescimento profissional através de um curso gratuito de Técnicas Básicas da Construção Civil, a ConstruFavela promove networking com especialistas e expositores renomados.
             O objetivo é fortalecer e capacitar os moradores das comunidades, proporcionando impacto positivo e desenvolvimento local.</p>
           <button className="bg-[#EB591E] text-xl text-white rounded-md md:w-1/3 p-2">Inscreva-se</button>
         </div>
@@ -46,7 +46,7 @@ export default function Home() {
         <div className="w-full flex flex-col md:flex-row md:p-10">
           <div className="w-full md:w-1/2 md:px-10 text-center md:text-left py-3">
             <h1 className="text-[#EB591E] text-2xl font-bold ">Nossos números</h1>
-            <p>Confira os resultados alcançados na última edição do ConstruFavela:</p>
+            <p className="text-black">Confira os resultados alcançados na última edição do ConstruFavela:</p>
           </div>
           <div className="grid grid-cols-2 grid-rows-2 gap-4 w-full md:w-1/2">
             <div className="flex items-center justify-center border p-4 bg-gray-200 flex-col md:flex-row rounded-md">
@@ -55,7 +55,7 @@ export default function Home() {
               </div>
               <div className=" w-full md:w-2/3 items-center text-center flex flex-col">
                 <h3 className="text-3xl font-bold text-[#EB591E] ">1.200+</h3>
-                <p>pessoas passaram pela feira</p>
+                <p className="text-black">pessoas passaram pela feira</p>
               </div>
             </div>
             <div className="flex items-center justify-center border p-4 bg-gray-200 flex-col md:flex-row rounded-md">
@@ -64,7 +64,7 @@ export default function Home() {
               </div>
               <div className="w-full md:w-2/3 items-center text-center flex flex-col">
                 <h3 className="text-3xl font-bold text-[#EB591E] ">500+</h3>
-                <p>Cestas básicas distribuídas na feira </p>
+                <p className="text-black">Cestas básicas distribuídas na feira </p>
               </div>
             </div>
             <div className="flex items-center justify-center border p-4 bg-gray-200 flex-col md:flex-row rounded-md">
@@ -73,7 +73,7 @@ export default function Home() {
               </div>
               <div className="w-full md:w-2/3 items-center text-center flex flex-col">
                 <h3 className="text-3xl font-bold text-[#EB591E] ">435</h3>
-                <p>pessoas assistiram as palestras</p>
+                <p className="text-black">pessoas assistiram as palestras</p>
               </div>
             </div>
             <div className="flex items-center justify-center border p-4 bg-gray-200 flex-col md:flex-row rounded-md">
@@ -82,7 +82,7 @@ export default function Home() {
               </div>
               <div className="w-full md:w-2/3 items-center text-center flex flex-col">
                 <h3 className="text-3xl font-bold text-[#EB591E] ">145</h3>
-                <p>pessoas assistiram aos workshops</p>
+                <p className="text-black">pessoas assistiram aos workshops</p>
               </div>
             </div>
           </div>
@@ -95,7 +95,7 @@ export default function Home() {
       <div className="w-full flex flex-col md:flex-row md:px-20 bg-white py-10">
         <div className="md:w-1/2 md:p-10 gap-10 order-last md:text-left text-center md:order-first p-5">
           <h3 className="text-4xl  font-bold text-[#EB591E]">Nosso Fundador</h3>
-          <p className="text-xl py-4">
+          <p className="text-xl py-4 text-black">
             Carlos Silva é um dos co-criadores do projeto social &quot;Cores da Favela&quot;, uma iniciativa transformadora que oferece melhorias habitacionais para famílias em situação de alta vulnerabilidade social. O projeto não apenas se dedica à reforma e adequação das moradias dessas comunidades, mas também oferece capacitação profissional e oportunidades de emprego e renda. O objetivo é promover uma mudança sustentável na vida dos participantes.
             <br /><br />
             Carlos Silva e sua equipe trabalham para criar um impacto duradouro, abordando as necessidades básicas e criando condições para o desenvolvimento pessoal e econômico das famílias atendidas. O projeto reflete um forte compromisso com a inclusão social e a justiça econômica.
@@ -107,7 +107,7 @@ export default function Home() {
       </div>
       <Footer />
       <a
-        href="https://wa.me/5511999999999" // Substitua pelo seu número de WhatsApp no formato internacional
+        href="https://wa.me/5511962696366" // Substitua pelo seu número de WhatsApp no formato internacional
         className="fixed bottom-5 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors"
         target="_blank"
         rel="noopener noreferrer"
