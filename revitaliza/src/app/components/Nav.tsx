daa3441f9d4550274fa33181eeb498365a109501
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-08-07 13:39:18 -0300

    Update Nav.tsx

--- revitaliza/src/app/components/Nav.tsx
+++ revitaliza/src/app/components/Nav.tsx
@@ -6,7 +6,7 @@ import G10 from "../../../public/g10.png";
 
 export default function NavBar() {
   return (
-    <nav className="bg-white border-gray-200 border-b shadow-lg dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 border-b shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse text-[#F95201]">
                   <Image src={Logo} alt="Flowbite Logo" width={50} height={50} />
