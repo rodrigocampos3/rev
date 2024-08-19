59065e8ff2eb057d879e3850d494346896357a82
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-08-19 16:49:50 -0300

    video removido
    video removido

--- revitaliza/src/app/components/Whats.tsx
+++ revitaliza/src/app/components/Whats.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/5511999999999" // Substitua pelo seu número de WhatsApp no formato internacional
+      href="https://wa.me/5511999999999"
       className="fixed bottom-5 right-5 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors"
       target="_blank"
       rel="noopener noreferrer"
