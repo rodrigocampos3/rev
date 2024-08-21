7f4c0b434c96a3087a37167044f2e2a6bc82339b
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-08-21 14:36:19 -0300

    Update Nav.tsx

--- revitaliza/src/app/components/Nav.tsx
+++ revitaliza/src/app/components/Nav.tsx
@@ -52,6 +52,9 @@ export default function NavBar() {
               <a href="https://cores.construfavela.com.br" className="block py-2 px-3 text-[#F95201] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cores da Favela</a>
             </li>
             <li>
+              <a href="https://institutorevitaliza.construfavela.com.br" className="block py-2 px-3 text-[#F95201] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Instituto Revitaliza</a>
+            </li>
+            <li>
               <a href="https://revitaliza.construfavela.com.br" className="block py-2 px-3 text-[#F95201] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Revitaliza</a>
             </li>
           </ul>
